c1d7427300da7caec4760a34373325432de58843
Казис <yuka95@inbox.ru>
2022-04-13 20:59:14 +1000

    2. Каркас шахматной доски

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,15 +7,6 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Ignore
-    @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
-    }
-
     @Test (expected = ImpossibleMoveException.class)
     public void moveImpossibleMoveException()
         throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
